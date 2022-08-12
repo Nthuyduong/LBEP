b52ebceff6055d29211ecbed8f2fe74283e57669
Nthuyduong <nguyenthuyduong15113@gmail.com>
2022-08-12 11:26:08 +0700

    baitap

---
+++ no08/function.h
@@ -0,0 +1,32 @@
+//Tinh tong chu so cua 1 so nguyen n
+int sum(int n){
+	int i,s=0;
+	while (n>0){
+		i=n%10;;
+		s=s+i;
+		n=n/10;
+	}return s;
+}
+//tinh chu vi tam giac
+int tinhchuvi(int a, int b, int c){
+	int p= a+b+c;
+	return p;
+}
+//tim uoc chung max cua 2 so
+int uocmax(int a, int b){
+	int u;
+	for(u=a;u<=a;u--){
+		if(a%u==0 && b%u==0){
+			return u;
+		}
+	}return u;
+}
+//Tim boi chung nho nhat cua 2 so
+int boichungmin(int c, int d){
+	int i;
+	for(i=c;i<=c*d;i++){
+		if(i%c==0 && i%d==0){
+			return i;
+		}
+	}return i;
+}
--- no08/no01.cpp
+++ no08/no01.cpp
@@ -1,33 +1,22 @@
 #include <stdio.h>
-//Tinh tong chu so cua 1 so nguyen n
-int sum(int n){
-	int i,s=0;
-	while (n>0){
-		i=n%10;;
-		s=s+i;
-		n=n/10;
-	}return s;
-}
+#include "function.h"
+int main(){
+//tinh tong chu so cua 1 so nguyen n
+	int x=40;
+	int s=sum(x);
+	printf("Tong chu so cua so nguyen n = %d\n",s);
 //tinh chu vi tam giac
-int tinhchuvi(int a, int b, int c){
-	int p= a+b+c;
-	return p;
-}
-//tim uoc chung max cua 2 so
-int uocmax(int a, int b){
-	int u;
-	for(u=a;u<=a;u--){
-		if(a%u==0 && b%u==0){
-			return u;
-		}
-	}return u;
-}
-//Tim boi chung nho nhat cua 2 so
-int boichungmin(int c, int d){
-	int i;
-	for(i=c;i<=c*d;i++){
-		if(i%c==0 && i%d==0){
-			return i;
-		}
-	}return i;
+	int x1=5,y1=10,z1=8;
+	int cv=tinhchuvi(x1,y1,z1);
+	printf("Chu vi tam giac = %d\n",cv);
+//tim uoc chung max
+	int x2=20,y2=42;
+	int uc;
+	uc=uocmax(x2,y2);
+	printf("Uoc chung max cua %d và %d = %d\n",x2,y2,uc);
+//tim boi chung nho nhat
+	int x3=24,y3=12;
+	int bc;
+	bc=boichungmin(x3,y3);
+	printf("Boi chung nho nhat cua %d và %d = %d",x3,y3,bc);
 }
